3711c47bed01c9c3670d516d365d8090651793b4
JHY.Solutions <72576017+Jeffrey-hendell@users.noreply.github.com>
2026-02-10 00:06:05 -0400

    Change API base URL to production endpoint

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api/users',
+    baseURL: 'https://api-training-8cun.onrender.com/api/users',
     headers: {'Content-Type' : 'application/json'}
 });
 
